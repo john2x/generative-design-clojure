8c57860ef0fd87cf4359dc98bd68822cd19ae21d
John Louis Del Rosario <john2x@gmail.com>
2012-08-30 14:56:38 +0800

    Add save png on key-press

--- src/generative_design_clojure/principles/P_1_0_01/P_1_0_01.clj
+++ src/generative_design_clojure/principles/P_1_0_01/P_1_0_01.clj
@@ -1,5 +1,19 @@
 (ns generative-design-clojure.principles.P_1_0_01.P_1_0_01
-  (:use quil.core))
+  (:use quil.core)
+  (:import java.util.Calendar))
+
+; P_1_0_01.clj
+;
+; Licensed under the Apache License, Version 2.0
+
+; changing colors and size by moving the mouse
+;
+; MOUSE
+; position x    : size
+; position y    : color
+;
+; KEYS
+; s             : save png
 
 (defn setup []
   (no-cursor))
@@ -10,10 +24,20 @@
   (no-stroke)
   (background (/ (mouse-y) 2.0) 100 100)
   (fill (- 360 (/ (mouse-y) 2.0)) 100 100)
-  (rect 360 360 (+ (mouse-x) 1) (+ (mouse-x) 1)))
+  (rect 360 360 (+ (mouse-x) 1) (+ (mouse-x) 1))
+  (text-size 20))
+
+(defn timestamp []
+  (let [now (Calendar/getInstance)]
+    (format "%1$ty%1$tm%1$td_%1$tH%1$tM%1$tS" now)))
+
+(defn key-press []
+  (if (= (str (raw-key)) "s")
+    (save-frame (str (timestamp) "_##.png"))))
 
 (defsketch P_1_0_01
            :title "P_1_0_01"
            :setup setup
            :draw draw
+           :key-pressed key-press
            :size [720 720])
