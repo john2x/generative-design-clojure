48bd2d9a4b837414ce126f4b55507929f44f3a55
John Louis Del Rosario <john2x@gmail.com>
2012-08-30 15:42:00 +0800

    Update quil version

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [quil "1.5.0"]]
+                 [quil "1.6.0"]]
   :main generative-design-clojure.core)
  
