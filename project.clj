d0d3446f2b0a07b414dabf2467d2650ef65c1bb9
John Louis Del Rosario <john2x@gmail.com>
2012-09-02 11:37:44 +0800

    Add generativedesign localrepo

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [quil "1.6.0"]]
+                 [quil "1.6.0"]
+                 [local/generativedesign "1.0.3"]]
+  :repositories {"project" "file:localrepo"}
   :main generative-design-clojure.core)
  
