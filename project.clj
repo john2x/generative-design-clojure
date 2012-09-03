0d9f86e0fd47cff9b48bb600cd4581362497907a
John Louis Del Rosario <john2x@gmail.com>
2012-09-03 11:47:16 +0800

    update project properties

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject generative-design-clojure "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "My attempt to learn Clojure and Processing at the same time."
+  :url "https://github.com/john2x/generative-design-clojure"
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [quil "1.6.0"]
                  [local/generativedesign "1.0.3"]]
